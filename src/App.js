a03dca16e265521303e524399c35c4594179489a
Manish Suthar <msutharmohit362@gmail.com>
2023-04-24 13:32:02 +0530

    Somes Changes are done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './Components/CssFiles/Navbar.css';
 import './Components/CssFiles/NewsItems.css';
 import './Components/CssFiles/NewsContainer.css';
+import './Components/CssFiles/ItemModal.css';
 import './App.css';
 
 import React, { Component } from 'react'
 import Navbar from './Components/JsFiles/Navbar';
 import News from './Components/JsFiles/News';
+import ItemModal from './Components/JsFiles/ItemModal';
 
 export default class App extends Component {
   render() {
     return (
       <div className='mainContainer'>
-        <Navbar />
-        <News/>
+        {/* <Navbar /> */}
+        {/* <News/> */}
+        <ItemModal/>
       </div>
     )
   }
---
+++ src/Components/JsFiles/ItemModal.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class ItemModal extends Component {
+  render() {
+    return (
+      <div className='itemModalContainer'>
+        <div className="itemModal">
+            Hello
+        </div>
+      </div>
+    )
+  }
+}
